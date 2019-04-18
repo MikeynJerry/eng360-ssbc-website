e29d8dbcaa3d8a7d69ffd14d97a9672bd005833d
3PIV <pprovins@vols.utk.ed>
2019-04-18 11:31:56 -0400

    Added Rudimentary Free Page

--- src/routes/Free/components/FreePage/FreePage.js
+++ src/routes/Free/components/FreePage/FreePage.js
@@ -1,9 +1,99 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Scrollbar from 'react-scrollbars-custom'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { Slide } from '@material-ui/core'
+
+const ArticleCard = ({ article, classes, image }) => (
+  <Card className={classes.card}>
+    <CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image={image}
+        title={article.title}
+        style={{ pointerEvents: 'none' }}
+      />
+      <CardContent style={{ minHeight: 154.4 }}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {article.title}
+        </Typography>
+        <Typography component="p">{article.description}</Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <a target="_blank" rel="noopener noreferrer" href={article.link}>
+        <Button size="small" color="primary">
+          Read More
+        </Button>
+      </a>
+    </CardActions>
+  </Card>
+)
+
+const articles = {
+  'pivot-grab': {
+    title: 'Pivot Grabbing for Dolts',
+    description:
+      'Getting snagged from half way across the stage? Well, you can do it too!',
+    link: 'https://www.nytimes.com/2019/04/18/us/politics/mueller-report.html',
+    image: 'https://www.ssbwiki.com/images/e/e2/Grab.jpg'
+  },
+  'keep-away': {
+    title: 'Dare to Nair: Stop Following Me!',
+    description:
+      'Is  your oppponent glued to you and invading your personal space? Neutral air is here to help!',
+    link: 'https://www.nytimes.com/2019/04/18/us/politics/mueller-report.html',
+    image: 'https://www.ssbwiki.com/images/d/d4/MarioNairSSBM.gif'
+  },
+  'best-music': {
+    title: 'What is the Best Smash Jam?',
+    description:
+      'Final Destination or Battlefields galore, which Smash tune is the best for a showdown? Here is our top ten!',
+    link: 'https://www.nytimes.com/2019/04/18/us/politics/mueller-report.html',
+    image: 'https://pbs.twimg.com/profile_images/727366040938668033/LJwv3Gd8.jpg'
+  }
+}
+
+const articleList = ['pivot-grab', 'keep-away', 'best-music']
 
 export const FreePage = ({ classes }) => (
-  <div className={classes.container}>
-    <span>FreePage Component</span>
+  <div>
+    <Scrollbar className={classes.scrollbar}>
+      <div className={classes.root}>
+        <div className={classes.tiles}>
+          {articleList.map((articleName, i) => {
+            const article = articles[articleName]
+            const image = article.image
+            return (
+              <Slide
+                key={articleName}
+                direction="up"
+                in
+                mountOnEnter
+                unmountOnExit
+                timeout={{ enter: 300 + 100 * i }}>
+                <ArticleCard
+                  article={article}
+                  image={image}
+                  classes={classes}
+                />
+              </Slide>
+            )
+          })}
+        </div>
+      </div>
+    </Scrollbar>
   </div>
 )
+
+FreePage.propTypes = {
+  classes: PropTypes.object.isRequired // from enhancer (withStyles)
+}
 
 export default FreePage
