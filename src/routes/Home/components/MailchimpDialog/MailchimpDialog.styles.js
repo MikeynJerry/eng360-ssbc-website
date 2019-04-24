eaa1de1e38d3a83926047a9be0cd9c0be63787a5
Jerry <MikeynJerry@gmail.com>
2019-04-24 01:33:21 -0400

    Added Scrollbar to Email Subscription Prompt

--- src/routes/Home/components/MailchimpDialog/MailchimpDialog.styles.js
+++ src/routes/Home/components/MailchimpDialog/MailchimpDialog.styles.js
@@ -6,5 +6,11 @@ export default theme => ({
     height: 300,
     margin: '0 auto',
     display: 'block'
+  },
+  scrollbar: {
+    width: '600px !important',
+    height: '690px !important',
+    display: 'flex',
+    flexDirection: 'row'
   }
 })
